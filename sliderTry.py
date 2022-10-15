daad508c30e11813df4abc6bcfd6af89a5b0c534
sdlauer <sdlauer@gmail.com>
2022-10-15 15:42:46 -0400

    try colors

--- sliderTry.py
+++ sliderTry.py
@@ -1,3 +1,8 @@
 import streamlit as st
+primaryColor="#F63366"
+backgroundColor="#000000"
+secondaryBackgroundColor="#F0F2F6"
+textColor="#262730"
+font="times"
 age = st.slider('How old are you?', 0, 130, 25)
 st.write("I'm ", age, 'years old')
