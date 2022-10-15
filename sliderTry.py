825e50e090c9f02b05261a79e6993503bc30b1b0
sdlauer <sdlauer@gmail.com>
2022-10-15 16:38:20 -0400

    trying Colors 2

--- sliderTry.py
+++ sliderTry.py
@@ -1,8 +1,55 @@
 import streamlit as st
-primaryColor="#F63366"
-backgroundColor="#000000"
-secondaryBackgroundColor="#F0F2F6"
-textColor="#262730"
-font="times"
-age = st.slider('How old are you?', 0, 130, 25)
+# import matplotlib.colors as mcolors
+import numpy as np
+import matplotlib.pyplot as plt
+plt.viridis()
+
+
+age = st.slider('How old are you?', 0, 120, 25)
+
 st.write("I'm ", age, 'years old')
+# rgb(253, 231, 37)
+#
+# #fde725
+# rgb(208, 225, 28)
+#
+# #d0e11c
+# rgb(160, 218, 57)
+#
+# #a0da39
+# rgb(115, 208, 86)
+#
+# #73d056
+# rgb(74, 193, 109)
+#
+# #4ac16d
+# rgb(45, 178, 125)
+#
+# #2db27d
+# rgb(31, 161, 135)
+#
+# #1fa187
+# rgb(33, 145, 140)
+#
+# #21918c
+# rgb(39, 127, 142)
+#
+# #277f8e
+# rgb(46, 110, 142)
+#
+# #2e6e8e
+# rgb(54, 92, 141)
+#
+# #365c8d
+# rgb(63, 71, 136)
+#
+# #3f4788
+# rgb(70, 50, 126)
+#
+# #46327e
+# rgb(72, 27, 109)
+#
+# #481b6d
+# rgb(68, 1, 84)
+#
+# #440154
