427f2985ea7f8e5a210a4b0beeaeff57b1815be8
sdlauer <sdlauer@gmail.com>
2022-10-16 15:18:24 -0400

    small update

--- tryPyvis.py
+++ tryPyvis.py
@@ -1,15 +1,13 @@
-# import streamlit
+from pyvis import network as net
+import streamlit as st
+from stvis import pv_static
 
-# from pyvis import network as net
-# import streamlit as st
-# from stvis import pv_static
-#
-# g=net.Network(height='500px', width='500px',heading='')
-# g.add_node(1)
-# g.add_node(2)
-# g.add_node(3)
-# g.add_edge(1,2)
-# g.add_edge(2,3)
-#
-#
-# pv_static(g)
+g=net.Network(height='500px', width='500px',heading='')
+g.add_node(1)
+g.add_node(2)
+g.add_node(3)
+g.add_edge(1,2)
+g.add_edge(2,3)
+
+
+pv_static(g)
