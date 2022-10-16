bb2de06499b05ef745728333424edaad23ac1ba1
sdlauer <sdlauer@gmail.com>
2022-10-16 11:38:59 -0400

    added some imports

--- tryPyvis.py
+++ tryPyvis.py
@@ -1,40 +1,5 @@
 # import streamlit
-# from streamlit_agraph import agraph, Node, Edge, Config
-# # https://github.com/ChrisDelClea/streamlit-agraph
-# nodes = []
-# edges = []
-# nodes.append( Node(id="Spiderman",
-#                    label="Peter Parker",
-#                    size=25,
-#                    shape="circularImage",
-#                    image="http://marvel-force-chart.surge.sh/marvel_force_chart_img/top_spiderman.png")
-#             ) # includes **kwargs
-# nodes.append( Node(id="Captain_Marvel",
-#                    size=25,
-#                    shape="circularImage",
-#                    image="http://marvel-force-chart.surge.sh/marvel_force_chart_img/top_captainmarvel.png")
-#             )
-# edges.append( Edge(source="Captain_Marvel",
-#                    label="friend_of",
-#                    target="Spiderman",
-#                    # **kwargs
-#                    ))
-# edges.append(Edge(target="Captain_Marvel",
-#                label="friend_of",
-#                source="Spiderman",
-#                # **kwargs
-#                )
-#         )
-#
-#
-# config = Config(width=500,
-#                 height=500,
-#                 # **kwargs
-#                 )
-#
-# return_value = agraph(nodes=nodes,
-#                       edges=edges,
-#                       config=config)
+
 from pyvis import network as net
 import streamlit as st
 from stvis import pv_static
