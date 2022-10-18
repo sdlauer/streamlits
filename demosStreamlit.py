9ac716644768e0113626dfd08f7c44fd1a399273
sdlauer <sdlauer@gmail.com>
2022-10-18 10:23:01 -0400

    real req?!

--- demosStreamlit.py
+++ demosStreamlit.py
@@ -39,27 +39,33 @@ st.graphviz_chart('''
 #     print("Hello, Streamlit!")'''
 # st.code(code, language='python')
 # st.text('This is some text.')
-st.latex(r'''
-    a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
-    \sum_{k=0}^{n-1} ar^k =
-    a \left(\frac{1-r^{n}}{1-r}\right)
-    ''')
+col1, col2 = st.columns([2,3])
+with col1:
+
 
 # plt.viridis()
-start_color, end_color = st.select_slider(
-    'Select a range of color wavelength',
-    options=['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'],
-    value=('red', 'blue'))
-st.write('You selected wavelengths between', start_color, 'and', end_color)
-color1 = st.select_slider(
-    'Select a color of the rainbow',
-    options=['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
-st.write('My favorite color is', color1)
+    start_color, end_color = st.select_slider(
+        'Select a range of color wavelength',
+        options=['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'],
+        value=('red', 'blue'))
+    c12 = st.write('You selected wavelengths between', start_color, 'and', end_color)
+    # color1 = st.select_slider(
+    #     'Select a color of the rainbow',
+    #     options=['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
+    color1 = st.radio(
+        'Select a color of the rainbow',
+        ('red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'))
+    c13 = st.write('My favorite color is', color1)
+    c11 = st.latex(r'''
+        a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
+        \sum_{k=0}^{n-1} ar^k =
+        a \left(\frac{1-r^{n}}{1-r}\right)
+        ''')
+with col2:
+    age = st.slider('How old are you?', 0, 120, 25)
 
-age = st.slider('How old are you?', 0, 120, 25)
-
-st.write("I'm ", age, 'years old')
-st.markdown('Streamlit is **_really_ cool**.')
+    c22 = st.write("I'm ", age, 'years old')
+    c23 = st.markdown('Streamlit is **_really_ cool**.')
 
 
 # rgb(253, 231, 37)
