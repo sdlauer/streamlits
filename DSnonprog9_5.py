9bbf3727827df9605a381f025a892a2b593a5dad
sdlauer <sdlauer@gmail.com>
2022-10-30 05:33:20 -0400

    update req

--- DSnonprog9_5.py
+++ DSnonprog9_5.py
@@ -2,14 +2,13 @@ import streamlit as st
 import pandas as pd
 # import seaborn as sns
 import numpy as np
-import sklearn
 # import matplotlib.pyplot as plt
-# from sklearn.neighbors import KNeighborsClassifier
-# from sklearn.model_selection import train_test_split
-# from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score, precision_score, recall_score
 # from mlxtend.plotting import plot_decision_regions
-import sklearn
+
 
 hide = """
         <style>
@@ -33,8 +32,8 @@ cancer = cancer.replace(to_replace = ['M','B'],value = [int(1), int(0)])
 
 X = cancer[['Radius mean', 'Texture mean']]
 y = cancer[['Diagnosis']]
-X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.3, random_state=123)
-scaler = sklearn.preprocessing.StandardScaler()
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=123)
+scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
 images = {3: "KNeigh3.png", 5: "KNeigh5.png", 7: "KNeigh7.png", 9: "KNeigh9.png", 11: "KNeigh11.png", 13: "KNeigh13.png", 
@@ -48,7 +47,7 @@ with col1:
             [3, 5, 7, 9, 11, 13, 15, 17, 19]
         )
 
-        knn = sklearn.neighbors.KNeighborsClassifier(n_neighbors=k) 
+        knn = KNeighborsClassifier(n_neighbors=k) 
         knn.fit(X_train_scaled, np.ravel(y_train))
 
         y_pred = knn.predict(X_train_scaled)
